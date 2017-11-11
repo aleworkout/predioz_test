fcf1d688bbbedd742e82ade4d185900c764cf65b
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2017-11-11 21:05:10 +0000

    se elimino paperclip de production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  Paperclip.options[:command_path] = "/usr/bin"
-  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
