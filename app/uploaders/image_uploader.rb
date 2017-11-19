a845daefae26421baf628bbde8d0dc36afdc47fc
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2017-11-19 23:19:21 +0000

    dsadsadsa

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,21 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  require "mini_magick"
   
   storage :fog
   
+  include CarrierWave::MiniMagick
+
+  process :scale => [300, 300]
+
+  def scale(width, height)
+    process :resize_to_fit => [400, 400]
+  end
+
   version :thumb do
-    process resize_to_fill: [280, 280]
+    process :resize_to_fit => [50, 50]
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 
 
